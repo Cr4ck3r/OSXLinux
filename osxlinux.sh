d83d6a6c1a292f37c28a4e5b56c4f45e841fa1dc
Graham Edwards <gedwards@A1077-MBP15.hq.getpebble.com>
2015-02-13 16:59:57 -0800

    Hi

---
+++ osxlinux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
+brew doctor
+
+brew install binutils
+brew install diffutils
+brew install ed --default-names
+brew install findutils --default-names
+brew install gawk
+brew install gnu-indent --default-names
+brew install gnu-sed --default-names
+brew install gnu-tar --default-names
+brew install gnu-which --default-names
+brew install gnutls --default-names
+brew install grep --default-names
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
